a781069941bf31022ff8d73bc62b40ac9c16d093
adrianjleon <adrianjleong@gmail.com>
2022-04-02 13:25:54 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 
-#pytesseract.pytesseract.tesseract_cmd = r'C:\\Users\\slash\\miniconda3\\envs\\receptor-mensajes\\Library\\bin\\tesseract.exe'
-
+#pytesseract.pytesseract.tesseract_cmd = r'C:\\Users\\slash\\miniconda3\\envs\\listener\\Library\\bin\\tesseract.exe'
+# Datos obtenidos al crear cuenta en https://www.twilio.com/
 api_id = 2351345  # Client's API ID
-api_hash = 'a1a55fcc6b88f4741e75b5b9e29210bb'  # Client's API hash
-phone_id = '+5491132305926'  # Client's phone
+api_hash = ''  # Client's API hash
+phone_id = ''  # Client's phone
