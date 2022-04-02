4d1847d2975e785b66fb47d88c12a6b3bbcd65a7
adrianjleon <adrianjleong@gmail.com>
2022-04-02 13:19:54 -0300

    Files from backup

---
+++ channels_ids.py
@@ -0,0 +1,103 @@
+
+
+channels_info2 = {
+-427723780 : ['group_test'],
+-1001443563552: ['Making Traders'],
+
+-1001428656076 : ['Fx Pro Signals',
+                  '([A-Z]{6})\s(SELL|BUY)\s([0-9\.\s]+)(SL)\s([0-9\.\s]+)(TP)([0-9\.\s]+)'],
+
+-1001389962734: ['FOREX PROFITABLE'],
+-1001263683550 : ['Forex Analysis',
+                  '([A-Z]{6})\s(SELL|BUY)\s([0-9\.\s]+)[A-Z\s\D]*(SL)\s([0-9\.\s]+)[A-Z\s\D]*(TP)\s([0-9\.\s]+)'],
+
+-1001286154418 : ['ƑX PLANET PRO'],
+
+-1001282964599 : ['FOREX SIGNALS'],
+
+-1001324494985: ['IC FINANCE LTD',
+                 '([A-Z]{6})\s(SELL|BUY)\s+ENTRY\s([0-9\.\s]+)(SL)\s([0-9\.\s]+)(TP)\s([0-9\.\s]+)(TP)\s([0-9\.\s]+)(TP)\s([0-9\.\s]+)'],
+
+-1001124716426 : ['green pips findings',
+                  '([A-Z]{6})\s(SELL|BUY)@([0-9\.\s]+)(SL):([0-9\.\s]+)(TP):([0-9\.\s]+)'],
+
+-1001203106845 : ['Exclusive Technical Analysis'],
+
+-1001429410613: ['𝗖𝗿𝘆𝘀𝘁𝗮𝗹 𝗳𝗼𝗿𝗲𝘅',
+                 '([A-Z]{6})\s(SELL|BUY)\s([0-9\.\s]+)(SL)\s([0-9\.\s]+)(TL)\s([0-9\.\s]+)'],
+
+-1001423597055 : ['Royals Senales Forex',
+                  "(SELL|BUY)\s([0-9\.\s]+)(TP)\s([0-9\.\s]+){1,3}(TP)\s([0-9\.\s]+){1,3}(TP)\s([0-9\.\s]+){1,3}(SL)\s([0-9\.\s]+)\s"],
+
+-1001442837191 : ['Gold Forex'],
+-1001277598276 : ['Forex Pro Traders'],
+
+-1001308637603 : ['Gold(Xauusd) daily signals'],
+
+-1001161380459: ['VIP TRADING',
+                 '([A-Z]{6})\s(SELL|BUY)\s+ENTRY\s([0-9\.\s]+)(SL)\s([0-9\.\s]+)(TP)\s([0-9\.\s]+)(TP)\s([0-9\.\s]+)(TP)\s([0-9\.\s]+)'],
+
+-1001332324676 : ['MYV'],
+
+-1001140400949 : ['Dexter Forex',
+                  '([A-Z]{6})\s(SELL|BUY).*\s([0-9\.\s]+)(TP)\s@\s([0-9\.\s]+)(SL)\s@\s([0-9\.\s]+)'],
+
+-1001480356148 : ['DFS - FREE FOREX SIGNALS',
+                  '^[0-9]{3,4}\s\(FREE\)\s(SELL|BUY)\s[A-Z]+\s([A-Z]{6})\s@([0-9\.\s]+)(SL):\s([0-9\.\s]+)(TP):\s([0-9\.\s]+)(TP)\d:\s([0-9\.\s]+)(TP)\d:\s([0-9\.\s]+)$'],
+
+
+-1001233904337 : ['VIP BINARY SHORT TIME'],
+
+-1001235677912: ['FX_PipsPredators',
+                 '([A-Z]{6})\s(SELL|BUY)_([0-9\.\s]+)(SL)_([0-9\.\s]+)(TP)_([0-9\.\s]+)(TP)[\d.]_([0-9\.\s]+)'],
+
+-1001173235046 : ['VIP BINARY SHORT TIME 2'],
+-1001348616823 : ['Signal Doctor ICHIMOKU'],
+
+-1001421292945 : ['PRO FX SPINER',
+                  '([A-Z]{4,6})\s(SELL|BUY)\sNOW\s@([0-9\.\sa]+)(TP)\s@\s([0-9\.\sa]+)(TP)\d\s@\s*([0-9\.\sa]+)(SL)\s@\s([0-9\.\sa]+)'],
+
+-1001456342198 : ['MrCat206 - FREE Forex',
+                  '([A-Z]{6})\s(SELL|BUY)\s(SL)\s([0-9\.\s]+)'],
+
+-1001481594439 : ['Fx Pips Master'],
+
+-1001156294702 : ['City Morgan FX'],
+
+-1001355784993 : ['Horizon Forex',
+                  '([A-Z]{6})\s(SELL|BUY)\s([0-9\.\s]+)(SL)\s([0-9\.\s]+)(TP)\d\s([0-9\.\s]+)(TP)\d\s([0-9\.\s]+)(TP)\d\s([A-Z0-9\.\s]+)'],
+
+474663203 : ['Horizon Forex',
+             '([A-Z]{6})\s(SELL|BUY)\s([0-9\.\s]+)(SL)\s([0-9\.\s]+)(TP)\d\s([0-9\.\s]+)(TP)\d\s([0-9\.\s]+)(TP)\d\s([A-Z0-9\.\s]+)'],
+
+-1001470308804 : ['IC FINANCE LTD REVIWES'],
+-1001202243568 : ['ROCKET SIGNALS INDEX'],
+-1001214852164 : ['Trading for a Living'],
+
+1037795435 : ['B&F SUPPORT'],
+
+-1001302974598 : ['XM MT4 Daily Signals'],
+-1001355784993 : ['Horizon Forex'],
+}
+
+
+# matches = pattern.search(message)
+# print("patron aqui\n")
+#
+# print(matches[2])
+
+#binary_pattern = "([A-Z]{6})\s(SELL|BUY)\s+ENTRY\s([0-9\.\s]+)(SL)\s([0-9\.\s]+)(TP)\s([0-9\.\s]+)(TP)\s([0-9\.\s]+)(TP)\s([0-9\.\s]+)"
+
+#Pips_world_Fx_pattern = '(SELL|BUY)\s([0-9\.\s]+)(TP){1}.{0,1}\s([0-9\.\s]+)(TP){0,1}.{0,1}\s([0-9\.\s]+)(TP){0,1}\d{0,1}.{0,1}\s([0-9\.\s]+){0,1}(SL)\D+\s([0-9\.]+)'
+
+#green_pips_finder = ([A-Z]{6})\s(SELL|BUY)@([0-9\.\s]+)(SL):([0-9\.\s]+)(TP):([0-9\.\s]+)
+
+
+
+
+# FX pro  signals ="""
+# XAUUSD BUY 1872
+# SL 1850
+# TP 1885
+#
+# """
---
+++ start.bat
@@ -0,0 +1,12 @@
+cmd /k "cd C:\Users\slash\Desktop\projects\listener & conda activate receptor-mensajes & python.exe main.py"
+
+
+
+
+
+
+
+
+
+ 
+
